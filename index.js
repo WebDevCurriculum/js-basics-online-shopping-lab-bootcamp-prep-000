75dfaee8392879771dde3f609b4fbf08b641ca86
Charity Youngblood <youngblood.charity@gmail.com>
2017-09-26 00:25:35 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ /* the item that will be passed through is a string*/
+var itemName;
+ /*itemPrice should be randomly generated integer between 1 and 100 */
+ var itemPrice = Math.floor(Math.random() * 100) + 1;
+ /*create one key value pair - itemName:itemPrice */
+ 
+ var itemInfo = [{}];
+  itemInfo["item"] = "itemPrice";
+  /*for(var i = 0; i < itemInfo.length; i++) {
+    itemInfo.push(item); */
+  }
+    /* item is a string. How will you add the string to the object */
+
 }
+
 
 function viewCart() {
   // write your code here

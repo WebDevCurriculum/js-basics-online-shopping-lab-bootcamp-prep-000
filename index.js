3a328d5e99bc26ba6d07c8ae0380ea52e9f66e0f
Charity Youngblood <youngblood.charity@gmail.com>
2017-09-26 23:07:38 +0000

    Done.

--- index.js
+++ index.js
@@ -12,20 +12,36 @@ function setCart(c) {
 function addToCart(itemName) {
  // write your code here
  /* the item that will be passed through is a string*/
+ /* create a new object */
 var itemObject = {};
+/*itemPrice should be randomly generated integer between 1 and 100 */
 var itemPrice = Math.floor(Math.random() * 100) + 1;
-itemObject[itemName] = itemPrice
- /*itemPrice should be randomly generated integer between 1 and 100 */
- /*create one key value pair - itemName:itemPrice */
+/*create one key value pair - itemName:itemPrice */
+itemObject[itemName] = itemPrice;
  /* add items to cart */
     cart.push(itemObject);
     console.log(`${itemName} has been added to your cart.`);
     return cart;
- }
+}
 
 function viewCart() {
   // write your code here
+/*price that is in existing cart */
+/* [{lemons:54}] */
+  if(cart.length == 0) {
+  console.log("Your shopping cart is empty.");
 }
+  else if(cart.length > 0){
+    var item = cart[0];
+    var itemName = Object.keys(item)[0];
+    var itemPrice = item[itemName];
+    console.log(`In your cart, you have ${itemName} at $${itemPrice}.`);
+  }
+
+}
+
+
+
 
 function total() {
   // write your code here

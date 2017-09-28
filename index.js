045a9a95ec1fddc53aeaa85185f9846aa5ee56fa
Charity Youngblood <youngblood.charity@gmail.com>
2017-09-28 16:56:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ for(var i = 0; i < cart.length; i++) {
   if(cart.length == 0) {
     console.log("Your shopping cart is empty.");
   }
-  else if(cart.length == 1){
+   if(cart.length == 1){
   console.log(`In your cart, you have ${itemName} at $${itemPrice}.`);
 }
   else if(cart.length == 2) {
